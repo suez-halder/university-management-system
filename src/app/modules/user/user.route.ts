4c8f5cde2f6180a15ee3d89f02e453b381ea4677
suezhalder <szhldr@gmail.com>
2023-12-18 18:48:15 +0600

    createFaculty, createAdmin [DONE]

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@
 
 import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
+import { createAdminValidationSchema } from '../admin/admin.validation';
+import { createFacultyValidationSchema } from '../faculty/faculty.validation';
 import { StudentValidations } from '../student/student.validation';
 import { UserControllers } from './user.controller';
 
@@ -11,6 +13,18 @@ router.post(
   '/create-student',
   validateRequest(StudentValidations.createStudentValidationSchema),
   UserControllers.createStudent,
+);
+
+router.post(
+  '/create-faculty',
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty,
+);
+
+router.post(
+  '/create-admin',
+  validateRequest(createAdminValidationSchema),
+  UserControllers.createAdmin,
 );
 
 export const UserRoutes = router;
